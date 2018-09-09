9ee0d74ce8a56856cc6700d822df4c7c80fa1bf3
Arbi Arzoumani <arbi@arzoumani.com>
2018-09-09 12:40:31 -0400

    Cleanup: Code in main class now follows (most of) PSR-1 coding standards and PSR-2 Style guide.

--- easy-footnotes.php
+++ easy-footnotes.php
@@ -25,187 +25,178 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-class easyFootnotes {
+class easyFootnotes
+{
 
-	// Add label option using add_option if it does not already exist
-	public $footnotes = array();
-	public $footnoteCount = 0;
-	public $prevPost;
-	public $footnoteOptions;
+    // Add label option using add_option if it does not already exist
+    public $footnotes = array();
+    public $footnoteCount = 0;
+    public $prevPost;
+    public $footnoteOptions;
 
-	private $footnoteSettings = array(
-			'footnoteLabel' => 'Footnotes',
-			'useLabel' => false,
-			'hide_easy_footnote_after_posts' => false,
-			'show_easy_footnote_on_front' => false
-		);
+    private $footnoteSettings = array(
+            'footnoteLabel' => 'Footnotes',
+            'useLabel' => false,
+            'hide_easy_footnote_after_posts' => false,
+            'show_easy_footnote_on_front' => false
+        );
 
-	public function __construct() {
-		
+    public function __construct()
+    {
+        add_option('easy_footnotes_options', $this->footnoteSettings);
+        add_shortcode('note', array($this, 'easy_footnote_shortcode'));
+        add_filter('the_content', array($this, 'easy_footnote_after_content'), 20);
+        add_filter('the_content', array($this, 'easy_footnote_reset'), 999);
+        add_action('wp_enqueue_scripts', array($this, 'register_qtip_scripts'));
+        add_action('admin_menu', array($this, 'easy_footnotes_admin_actions'));
+        add_action('admin_enqueue_scripts', array($this, 'easy_footnotes_admin_scripts'));
 
-		add_option('easy_footnotes_options', $this->footnoteSettings);
-		add_shortcode( 'note', array($this, 'easy_footnote_shortcode') );
-		add_filter('the_content', array($this, 'easy_footnote_after_content'), 20);
-		add_filter('the_content', array($this, 'easy_footnote_reset'), 999);
-		add_action('wp_enqueue_scripts', array($this, 'register_qtip_scripts'));
-		add_action('admin_menu', array($this, 'easy_footnotes_admin_actions'));
-		add_action( 'admin_enqueue_scripts', array($this, 'easy_footnotes_admin_scripts') );
-		add_action('updated_option', array($this, 'easy_footnotes_options_updated'), 10, 3);
+        $this->footnoteOptions = get_option('easy_footnotes_options');
+    }
 
-		$this->footnoteOptions = get_option('easy_footnotes_options');
+    public function register_qtip_scripts()
+    {
+        wp_register_script('imagesloaded', plugins_url('/assets/qtip/imagesloaded.pkgd.min.js', __FILE__), null, false, true);
+        wp_register_script('qtip', plugins_url('/assets/qtip/jquery.qtip.min.js', __FILE__), array('jquery', 'imagesloaded'), false, true);
+        wp_register_script('qtipcall', plugins_url('/assets/qtip/jquery.qtipcall.js', __FILE__), array('jquery', 'qtip'), false, true);
+        wp_register_style('qtipstyles', plugins_url('/assets/qtip/jquery.qtip.min.css', __FILE__), null, false, false);
+        wp_register_style('easyfootnotescss', plugins_url('/assets/easy-footnotes.css', __FILE__), null, false, false);
+    }
 
-	}
+    public function easy_footnote_shortcode($atts, $content = null)
+    {
+        if ($this->footnoteOptions['show_easy_footnote_on_front']) {
+            $b_ShowOnFront = is_front_page();
+        } else {
+            $b_ShowOnFront = false;
+        }
 
+        wp_enqueue_style('qtipstyles');
+        wp_enqueue_style('easyfootnotescss');
+        wp_enqueue_script('imagesloaded');
+        wp_enqueue_script('qtip');
+        wp_enqueue_script('qtipcall');
+        wp_enqueue_style('dashicons');
 
-	public function easy_footnotes_options_updated( $option_name, $old_value, $option_value ) {
-	        
-	    if (array_key_exists($option_name, $this->footnoteSettings)) {
-			$this->footnoteOptions = get_option('easy_footnotes_options');
-	    }
+        extract(shortcode_atts(array(
+        ), $atts));
 
-	}
+        $post_id = get_the_ID();
 
-	public function register_qtip_scripts() {
-		wp_register_script( 'imagesloaded', plugins_url( '/assets/qtip/imagesloaded.pkgd.min.js' , __FILE__ ), null, false, true );
-		wp_register_script( 'qtip', plugins_url( '/assets/qtip/jquery.qtip.min.js' , __FILE__ ), array('jquery', 'imagesloaded'), false, true );
-		wp_register_script( 'qtipcall', plugins_url( '/assets/qtip/jquery.qtipcall.js' , __FILE__ ), array('jquery', 'qtip'), false, true );
-		wp_register_style( 'qtipstyles', plugins_url( '/assets/qtip/jquery.qtip.min.css' , __FILE__ ), null, false, false );
-		wp_register_style( 'easyfootnotescss', plugins_url( '/assets/easy-footnotes.css' , __FILE__ ), null, false, false );
-	}
+        $content = do_shortcode($content);
 
-	public function easy_footnote_shortcode($atts, $content = null) {
+        $count = $this->footnoteCount;
+        
+        // Increment the counter
+        $count++;
 
-		$footnoteOptions = $this->footnoteOptions; //get_option('easy_footnotes_options');
+        // Set the footnoteCount (This whole process needs reworked)
+        $this->footnoteCount = $count;
 
-		$b_ShowOnFront = false;
+        //$this->easy_footnote_count($this->footnoteCount, get_the_ID());
+        $this->easy_footnote_content($content);
 
-		if ( isset($footnoteOptions['show_easy_footnote_on_front']) && $footnoteOptions['show_easy_footnote_on_front'] ) {
-			$b_ShowOnFront = is_front_page();
-		}
+        if ((is_singular() || $b_ShowOnFront) && is_main_query()) {
+            $footnoteLink = '#easy-footnote-bottom-'.$this->footnoteCount.'-'.$post_id;
+            ;
+        } else {
+            $footnoteLink = get_permalink(get_the_ID()).'#easy-footnote-bottom-'.$this->footnoteCount.'-'.$post_id;
+        }
 
-		wp_enqueue_style( 'qtipstyles' );
-		wp_enqueue_style( 'easyfootnotescss' );
-		wp_enqueue_script( 'imagesloaded' );
-		wp_enqueue_script( 'qtip' );
-		wp_enqueue_script( 'qtipcall' );
-		wp_enqueue_style( 'dashicons' );
+        $footnoteContent = "<span id='easy-footnote-".esc_attr($this->footnoteCount).'-'.$post_id."' class='easy-footnote-margin-adjust'></span><span class='easy-footnote'><a href='".esc_url($footnoteLink)."' title='".htmlspecialchars($content, ENT_QUOTES)."'><sup>".esc_html($this->footnoteCount)."</sup></a></span>";
 
-		extract (shortcode_atts(array(
-		), $atts));
+        return $footnoteContent;
+    }
 
-		$post_id = get_the_ID();
+    public function easy_footnote_content($content)
+    {
+        $this->footnotes[$this->footnoteCount] = $content;
 
-		$content = do_shortcode($content);
+        return $this->footnotes;
+    }
 
-		$count = $this->footnoteCount;
-		
-		// Increment the counter
-		$count++;
+    public function easy_footnote_count($count, $currentPost)
+    {
+        if ($this->prevPost != $currentPost) {
+            $count = 0;
+        }
 
-		// Set the footnoteCount (This whole process needs reworked)
-		$this->footnoteCount = $count;
+        $this->prevPost = $currentPost;
 
-		//$this->easy_footnote_count($this->footnoteCount, get_the_ID());
-		$this->easy_footnote_content($content);
+        $count++;
 
-		if (( is_singular() || $b_ShowOnFront ) && is_main_query()) {
-			$footnoteLink = '#easy-footnote-bottom-'.$this->footnoteCount.'-'.$post_id;;
-		} else {
-			$footnoteLink = get_permalink(get_the_ID()).'#easy-footnote-bottom-'.$this->footnoteCount.'-'.$post_id;
-		}
+        $this->footnoteCount = $count;
 
-		$footnoteContent = "<span id='easy-footnote-".esc_attr($this->footnoteCount).'-'.$post_id."' class='easy-footnote-margin-adjust'></span><span class='easy-footnote'><a href='".esc_url($footnoteLink)."' title='".htmlspecialchars($content, ENT_QUOTES)."'><sup>".esc_html($this->footnoteCount)."</sup></a></span>";
+        return $this->footnoteCount;
+    }
 
-		return $footnoteContent;
-	}
+    public function easy_footnote_after_content($content)
+    {
+        if ($this->footnoteOptions['hide_easy_footnote_after_posts']) {
+            return $content;
+        }
 
-	public function easy_footnote_content($content) {
-		$this->footnotes[$this->footnoteCount] = $content;
+        if ($this->footnoteOptions['show_easy_footnote_on_front']) {
+            $b_ShowOnFront = is_front_page();
+        } else {
+            $b_ShowOnFront = false;
+        }
 
-		return $this->footnotes;
-	}
+        if ((is_singular() || $b_ShowOnFront) && is_main_query()) {
+            $footnotesInsert = $this->footnotes;
 
-	public function easy_footnote_count($count, $currentPost) {
-		if ($this->prevPost != $currentPost) {
-			$count = 0;
-		}
+            $footnoteCopy = '';
 
-		$this->prevPost = $currentPost;
+            $useLabel = $this->footnoteOptions['useLabel'];
+            $efLabel = $this->footnoteOptions['footnoteLabel'];
 
-		$count++;
+            $post_id = get_the_ID();
 
-		$this->footnoteCount = $count;
+            foreach ($footnotesInsert as $count => $footnote) {
+                $footnoteCopy .= '<li class="easy-footnote-single"><span id="easy-footnote-bottom-'.esc_attr($count).'-'.$post_id.'" class="easy-footnote-margin-adjust"></span>'.wp_kses_post($footnote).'<a class="easy-footnote-to-top" href="'.esc_url('#easy-footnote-'.$count.'-'.$post_id).'"></a></li>';
+            }
+            if (!empty($footnotesInsert)) {
+                if ($useLabel === true) {
+                    $content .= '<div class="easy-footnote-title"><h4>'.esc_html($efLabel).'</h4></div><ol class="easy-footnotes-wrapper">'.$footnoteCopy.'</ol>';
+                } else {
+                    $content .= '<ol class="easy-footnotes-wrapper">'.$footnoteCopy.'</ol>';
+                }
+            }
+        }
+        
+        return $content;
+    }
 
-		return $this->footnoteCount;
-	}
+    /**
+     * Reset the footnote count and footnote array each time the_content has been run.
+     */
+    public function easy_footnote_reset($content)
+    {
+        $this->footnoteCount = 0;
 
-	public function easy_footnote_after_content($content) {
+        $this->footnotes = array();
 
-		$footnoteOptions = $this->footnoteOptions; //get_option('easy_footnotes_options');
-		
-		$b_ShowOnFront = false;
+        return $content;
+    }
 
-		if ( isset($footnoteOptions['hide_easy_footnote_after_posts']) && $footnoteOptions['hide_easy_footnote_after_posts'] ) {
-			return $content;
-		}
+    // Functions to create Reading Time admin pages
+    public function easy_footnotes_admin()
+    {
+        include('easy-footnotes-admin.php');
+    }
 
-		if ( isset($footnoteOptions['show_easy_footnote_on_front']) && $footnoteOptions['show_easy_footnote_on_front'] ) {
-			$b_ShowOnFront = is_front_page();
-		}
+    public function easy_footnotes_admin_actions()
+    {
+        add_options_page("Easy Footnotes Settings", "Easy Footnotes", "manage_options", "easy-footnotes-settings", array($this, "easy_footnotes_admin"));
+    }
 
-		if ( ( is_singular() || $b_ShowOnFront ) && is_main_query() ) {
-			$footnotesInsert = $this->footnotes;
+    public function easy_footnotes_admin_scripts()
+    {
+        wp_enqueue_style('easy-footnotes-admin-styles', plugins_url('/assets/easy-footnotes-admin.css', __FILE__), '', '1.0.13');
 
-			$footnoteCopy = '';
-
-			$useLabel = $footnoteOptions['useLabel'];
-			$efLabel = $footnoteOptions['footnoteLabel'];
-
-			$post_id = get_the_ID();
-
-			foreach ($footnotesInsert as $count => $footnote) {
-				$footnoteCopy .= '<li class="easy-footnote-single"><span id="easy-footnote-bottom-'.esc_attr($count).'-'.$post_id.'" class="easy-footnote-margin-adjust"></span>'.wp_kses_post($footnote).'<a class="easy-footnote-to-top" href="'.esc_url('#easy-footnote-'.$count.'-'.$post_id).'"></a></li>';
-			}
-			if (!empty($footnotesInsert)) {
-				if ($useLabel === true) {
-					$content .= '<div class="easy-footnote-title"><h4>'.esc_html($efLabel).'</h4></div><ol class="easy-footnotes-wrapper">'.$footnoteCopy.'</ol>';
-				} else {
-					$content .= '<ol class="easy-footnotes-wrapper">'.$footnoteCopy.'</ol>';
-				}
-			}
-
-		}
-		
-		return $content;
-	}
-
-	/**
-	 * Reset the footnote count and footnote array each time the_content has been run.
-	 */
-	public function easy_footnote_reset($content) {
-		$this->footnoteCount = 0;
-
-		$this->footnotes = array();
-
-		return $content;
-	}
-
-	// Functions to create Reading Time admin pages
-	public function easy_footnotes_admin() {
-	    include('easy-footnotes-admin.php');
-	}
-
-	public function easy_footnotes_admin_actions() {
-		add_options_page("Easy Footnotes Settings", "Easy Footnotes", "manage_options", "easy-footnotes-settings", array($this, "easy_footnotes_admin"));
-	}
-
-	public function easy_footnotes_admin_scripts() {
-		wp_enqueue_style( 'easy-footnotes-admin-styles', plugins_url( '/assets/easy-footnotes-admin.css' , __FILE__ ), '', '1.0.13' );
-
-	    wp_register_script('easy-footnotes-admin-scripts', plugins_url( '/assets/js/easy-footnotes-admin.js' , __FILE__ ), array( 'jquery' ), '1.0.1' );
-		wp_enqueue_script( 'easy-footnotes-admin-scripts' );
-	}
-
+        wp_register_script('easy-footnotes-admin-scripts', plugins_url('/assets/js/easy-footnotes-admin.js', __FILE__), array( 'jquery' ), '1.0.1');
+        wp_enqueue_script('easy-footnotes-admin-scripts');
+    }
 }
 
 $easyFootnotes = new easyFootnotes();
