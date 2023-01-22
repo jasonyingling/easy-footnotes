c214ff87eaf36a9a1dbf7fdbddfe0ee8151d9e5b
Alex Lion <learnwithalex@gmail.com>
2023-01-22 01:13:04 -0800

    Improve I18N Issues (Based on 1.1.6)
    - This plugin’s `Requires at least` is below 4.6. so you need to set a Text Domain in **easy-footnotes.php**, and it needs to be `easy-footnotes` to internationalize your plugin correctly! Please refer to this [official article](https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/#text-domains).
    - Requires at least (3.0.1) is below 4.6, so a `load_plugin_textdomain` is **needed**. Please make sure you load it at a certain point in your plugin. Please refer to this [official article](https://developer.wordpress.org/plugins/internationalization/how-to-internationalize-your-plugin/#loading-text-domain). I DO NOT make this modification because I don't know your function naming rule, so please do it by yourself.
    - Fix the UI string with wrong text domain.

--- easy-footnotes.php
+++ easy-footnotes.php
@@ -3,6 +3,7 @@
  * Plugin Name: Easy Footnotes
  * Plugin URI: https://jasonyingling.me/easy-footnotes-wordpress/
  * Description: Easily add footnotes to your posts with a simple shortcode.
+ * Text Domain: easy-footnotes
  * Version: 1.1.6
  * Author: Jason Yingling
  * Author URI: https://jasonyingling.me
