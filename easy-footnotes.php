8c455e038591755afc0185c48c3a5ca385a1c64a
Jason Yingling <jyingling@hlkagency.com>
2024-09-16 12:21:26 -0500

    tagging 1.1.8

--- easy-footnotes.php
+++ easy-footnotes.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy Footnotes
  * Plugin URI: https://jasonyingling.me/easy-footnotes-wordpress/
  * Description: Easily add footnotes to your posts with a simple shortcode.
- * Version: 1.1.7
+ * Version: 1.1.8
  * Author: Jason Yingling
  * Author URI: https://jasonyingling.me
  * License: GPL2
